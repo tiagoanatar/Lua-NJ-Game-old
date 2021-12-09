715c824f892d081b3679ced590ce0b728a32e09f
tiagoanatar <35281635+tiagoanatar@users.noreply.github.com>
2021-12-08 23:44:20 -0300

    Fix combat call

--- enemy_comp_coli.lua
+++ enemy_comp_coli.lua
@@ -1,10 +1,4 @@
--- ///////////////////////////////////////////////////////////////
---// VARS
---///////////////////////////////////////////////////////////////
-
--- BEHAVIOR
-
--- index of emenies - control each moving turn
+-- enemy ref holder
 local enemy_ref
 
 -- change behavior 
@@ -21,13 +15,13 @@ end
 --///////////////////////////////////////////////////////////////
 
 local function comp_reset() 
-  for i,v in ipairs(enen_list) do
-    if enen_list[i].comp ~= "dead" then
-      enen_list[i].comp = state.enemy.comp_random[love.math.random(1, 2)]
-      enen_list[i].change_comp = "off" -- stop behavior change
+  for i,v in ipairs(state.enemy.list) do
+    if state.enemy.list[i].comp ~= "dead" then
+      state.enemy.list[i].comp = state.enemy.comp_random[love.math.random(1, 3)]
+      state.enemy.list[i].change_comp = "off" -- stop behavior change
     end
   end
-  player.comp_alert = 1
+  state.player.comp_alert = 1
 end
 
 -- ///////////////////////////////////////////////////////////////
@@ -35,11 +29,15 @@ end
 --///////////////////////////////////////////////////////////////
 
 -- reset enemy counter
-local function enen_cont_reset() for i,v in ipairs(state.enemy.counter) do state.enemy.counter[i].anim_numb = 1 end end
+local function enen_cont_reset() 
+  for i,v in ipairs(state.enemy.counter) do 
+    state.enemy.counter[i].anim_numb = 1 
+  end 
+end
 
 local function index_comp_reset()
 
-  if state.enemy.index_comp > #enen_list then
+  if state.enemy.index_comp > #state.enemy.list then
     state.turn = "off"
     state.enemy.index_comp = 1
     enen_cont_reset() -- limpa contador
@@ -62,7 +60,6 @@ local function index_comp_reset()
       comp_reset()
       state.enemy.alert_d_time = 0
       state.enemy.alert_d = "off"
-      state.enemy.alert_d_first = "on"
     end
     
   end
@@ -73,9 +70,7 @@ end
 --// ENEMY MOVE
 --///////////////////////////////////////////////////////////////
 
--- // change direction ////////////////////////////
--- /////////////////////////////////////////////////////////
-
+-- change direction 
 local function muda_dire(random)
 
   if random == true then
@@ -84,61 +79,51 @@ local function muda_dire(random)
     if rand_stop == 2 then enemy_ref.scaX = -1 end
   end
   if random == false then
-    if player.x > enemy_ref.x then enemy_ref.scaX = 1 end
-    if player.x < enemy_ref.x then enemy_ref.scaX = -1 end
+    if state.player.x > enemy_ref.x then enemy_ref.scaX = 1 end
+    if state.player.x < enemy_ref.x then enemy_ref.scaX = -1 end
   end
 
 end
 
--- // movement ////////////////////////////
--- /////////////////////////////////////////////////////////
-
+-- movement
 function move_max_zero() enemy_ref.m_max = 0 end -- used also on: move
 
-  local function enem_move()
+local function enem_move()
 
-    -- if close to hero stop
-    if ma_he(player,enemy_ref) == 45 and enemy_ref.comp == "alert_player" then 
-      move_max_zero()
-      -- attack function
-      state.combat.enemy_active = "on"
-      combate_update()
-    end
+  -- if close to hero stop
+  if ma_he(state.player,enemy_ref) == 45 and enemy_ref.comp == "alert_player" then 
+    move_max_zero()
+  end
 
-    -- if close do body stop
-    if ma_he(e_v_dead,enemy_ref) == 45 and enemy_ref.comp ~= "alert_desconf" and enemy_ref.comp ~= "alert_player" then 
-      move_max_zero()
-      -- muda direcao 
-      muda_dire(true)
-    end
+  -- if close do body stop
+  if ma_he(state.enemy.vision.dead,enemy_ref) == 45 and enemy_ref.comp ~= "alert_desconf" and enemy_ref.comp ~= "alert_player" then 
+    move_max_zero()
+    -- muda direcao 
+    muda_dire(true)
+  end
 
-    -- first time alert stop
-    if enemy_ref.comp == "alert_player" and state.enemy.alert_p_first == "on" and state.move.ref_index == 0 then 
-      move_max_zero()
-      state.enemy.alert_p_first = "off"
-    end 
+  --[[ first time alert stop
+  --if enemy_ref.comp == "alert_player" and state.enemy.alert_p_first == "on" and state.move.ref_index == 0 then 
+  --  move_max_zero()
+  --  state.enemy.alert_p_first = "off"
+  --end 
 
-    -- first time body alert stop
-    if enemy_ref.comp == "alert_body" and state.enemy.alert_p_first == "on" and state.move.ref_index == 0 then 
-      move_max_zero()
-      state.enemy.alert_p_first = "off"
-    end 
+  -- first time body alert stop
+  if enemy_ref.comp == "alert_body" and state.enemy.alert_p_first == "on" and state.move.ref_index == 0 then 
+    move_max_zero()
+    state.enemy.alert_p_first = "off"
+  end 
 
-    -- first time item alert stop
-    if enemy_ref.comp == "alert_item" and state.enemy.alert_p_first == "on" and state.move.ref_index == 0 then 
-      move_max_zero()
-      state.enemy.alert_p_first = "off"
-    end 
+  -- first time item alert stop
+  if enemy_ref.comp == "alert_item" and state.enemy.alert_p_first == "on" and state.move.ref_index == 0 then 
+    move_max_zero()
+    state.enemy.alert_p_first = "off"
+  end --]]
 
-  -----
   -- move
-  -----
   move_main(enemy_ref)
 
 end
-------------------
--- END - enem_move()
-------------------
 
 -- ///////////////////////////////////////////////////////////////
 --// DIRECAO PATHFIND
@@ -183,16 +168,16 @@ local function dire_diago_path()
       end
 
     end
-  end -- END FOR
+  end 
 
   -- se nao encontrar nada -- encerrar tudo
   if #tab == 0 then
     state.move.rand_path_diago = false
-  -- GLOBAL - alimentando posicao escolhida
+  -- alimentando posicao escolhida
     state.range.fim.x = enemy_ref.x
     state.range.fim.y = enemy_ref.y
 
-  -- GLOBAL - acabado o for desativa toda essa area
+  -- acabado o for desativa toda essa area
     state.range.path_end = "off"
       
     return
@@ -203,17 +188,16 @@ local function dire_diago_path()
       
     local random_pos = love.math.random(1, #tab)
 
-  -- GLOBAL - alimentando posicao escolhida
+  -- alimentando posicao escolhida
     state.range.fim.x = tab[random_pos].x
     state.range.fim.y = tab[random_pos].y
 
-  -- GLOBAL - acabado o for desativa toda essa area
+  -- acabado o for desativa toda essa area
     state.range.path_end = "off"
     tab = nil
   end
 
 end
------
 
 -- comp - alert_player
 local function alert_path(ttype)
@@ -226,21 +210,16 @@ local function alert_path(ttype)
   state.range.path_end = "off"
   
 end
------
 
--- 0.1 // setup ////////////////////////////
--- /////////////////////////////////////////////////////////
-
+-- 0.1 - setup
 local function setup_inicial()
 
-    enemy_ref.m_max = 10
-    local_var = "off" -- turn off setup
+  enemy_ref.m_max = 10
+  local_var = "off" -- turn off setup
 
 end
 
--- 0.2 // setup final ////////////////////////////
--- /////////////////////////////////////////////////////////
-
+-- 0.2  - setup final
 local function setup_final()
   
   if state.combat.enemy_active == "off" then
@@ -261,9 +240,7 @@ local function setup_final()
 
 end
 
--- 1 // comp - stop ////////////////////////////
--- /////////////////////////////////////////////////////////
-
+-- 1 - comp - stop
 local function comp_stop()
   
 -- muda direcao 
@@ -274,12 +251,12 @@ local function comp_stop()
 
 -- interrompendo mudança de comportamento
   if trocar_comp == 1 then enemy_ref.change_comp = "on" end
+  
+  setup_final()
 
 end
 
--- 2 // comp - path_diago ////////////////////////////
--- /////////////////////////////////////////////////////////
-
+-- 2 - comp - path_diago
 local function comp_path_diago()
 
   -- setup inicial
@@ -309,7 +286,7 @@ local function comp_path_diago()
   if enemy_ref.m_max == 0 then
 
     -- chances de trocar de comportamento
-    trocar_comp = love.math.random(1, 2) -- 1 chance em 2
+    trocar_comp = love.math.random(1, 7) 
     if trocar_comp == 1 then enemy_ref.change_comp = "on" end
     setup_final()
 
@@ -317,9 +294,7 @@ local function comp_path_diago()
 
 end 
 
--- 3 // comp - sleep ////////////////////////////
--- /////////////////////////////////////////////////////////
-
+-- 3 - comp - sleep
 local function comp_sleep()
 
 -- chances de trocar de comportamento
@@ -327,167 +302,157 @@ local function comp_sleep()
 
 -- interrompendo mudança de comportamento
   if trocar_comp == 1 then enemy_ref.change_comp = "on" end
+  
+  setup_final()
 
 end
 
--- 4 // comp - dead ////////////////////////////
--- /////////////////////////////////////////////////////////
-
+-- 4 - comp - dead
 local function comp_dead()
 
-    setup_final()
+  setup_final()
 -- chances de trocar de comportamento
-    trocar_comp = 0
+  trocar_comp = 0
 
 end
 
 -- ///////////////////////////////////////////////////////////////
---// ALERTA
+--// ALERT
 --///////////////////////////////////////////////////////////////
 
--- 1 // alert - descon ////////////////////////////
--- /////////////////////////////////////////////////////////
-
+-- 1 - alert - descon
 local function alert_descon()
 
--- setup inicial
--- ativa e desativa variaveis locais -- executado 1 vez por inimigo
-if local_var == "on" then
+  -- setup inicial
+  -- ativa e desativa variaveis locais -- executado 1 vez por inimigo
+  if local_var == "on" then
     setup_inicial()
-end
-
--- escolhendo a direcao
-if state.range.path_end == "stop" then -- GLOBAL
-    alert_path(player)
-end
-
--- ativa path find
-if state.range.path_end == "off" then -- GLOBAL
-    range_path_final(enemy_ref)
-end
-
--- so ativa somente quando o path find acabar suas opearacoes
-if state.range.path_end == "on" then -- GLOBAL
-    muda_dire(false) -- vira inimigo em direcao ao heroi
-    enem_move() -- movimento
-end 
-  
--- finalizacao do movimento
-if enemy_ref.m_max == 0 then
-    setup_final()
-end
-
-end
-------------------
--- END - alert_descon()
-------------------
-
--- 2 // alert - player ////////////////////////////
--- /////////////////////////////////////////////////////////
-
-local function alert_player()
-
--- setup inicial
--- ativa e desativa variaveis locais -- executado 1 vez por inimigo
-if local_var == "on" then
-    setup_inicial()
-end
-
--- escolhendo a direcao
-if state.range.path_end == "stop" then -- GLOBAL
-    if enemy_ref.comp == "alert_body" then
-        alert_path(e_v_dead)
-    elseif enemy_ref.comp == "alert_item" then
-        alert_path(e_v_item)
-    elseif enemy_ref.comp == 'alert_player' then
-        alert_path(player)
-    end
-end
-
--- ativa path find
-if state.range.path_end == "off" then -- GLOBAL
-    range_path_final(enemy_ref)
-end
-
--- so ativa somente quando o path find acabar suas opearacoes
-if state.range.path_end == "on" then -- GLOBAL
-
-  if enemy_ref.comp == "alert_body" then
-      -- nil
-  else
-      muda_dire(false) 
   end
 
-  enem_move() -- movimento
+  -- escolhendo a direcao
+  if state.range.path_end == "stop" then -- GLOBAL
+    alert_path(state.player)
+  end
 
-end 
- 
--- finalizacao do movimento
-if enemy_ref.m_max == 0 then
-  setup_final()
+  -- ativa path find
+  if state.range.path_end == "off" then -- GLOBAL
+    range_path_final(enemy_ref)
+  end
+
+  -- so ativa somente quando o path find acabar suas opearacoes
+  if state.range.path_end == "on" then -- GLOBAL
+    muda_dire(false) -- vira inimigo em direcao ao heroi
+    enem_move() -- movimento
+  end 
+
+  -- finalizacao do movimento
+  if enemy_ref.m_max == 0 then
+    setup_final()
+  end
+
 end
+
+-- 2 - alert - player
+local function alert_player()
+
+  -- setup inicial
+  -- ativa e desativa variaveis locais -- executado 1 vez por inimigo
+  if local_var == "on" then
+    setup_inicial()
+  end
+
+  -- escolhendo a direcao
+  if state.range.path_end == "stop" then -- GLOBAL
+    if enemy_ref.comp == "alert_body" then
+      alert_path(state.enemy.vision.dead)
+    elseif enemy_ref.comp == "alert_item" then
+      alert_path(state.enemy.vision.item)
+    elseif enemy_ref.comp == 'alert_player' then
+      alert_path(state.player)
+    end
+  end
+
+  -- ativa path find
+  if state.range.path_end == "off" then -- GLOBAL
+    range_path_final(enemy_ref)
+  end
+
+  -- so ativa somente quando o path find acabar suas opearacoes
+  if state.range.path_end == "on" then
+
+    if enemy_ref.comp == "alert_body" then
+      -- nil
+    else
+      muda_dire(false) 
+    end
+
+    enem_move() -- movimento
+
+  end 
+ 
+  -- finalizacao do movimento
+  if enemy_ref.m_max == 0 then
+    setup_final()
+  end
   
 end
-------------------
--- END - alert_player()
-------------------
 
 -- ///////////////////////////////////////////////////////////////
 --// COMPORTAMENTOS
 --///////////////////////////////////////////////////////////////
 
--- 0 // estrutura principal ////////////////////////////
--- /////////////////////////////////////////////////////////
+-- 0 - estrutura principal
+function enemy_comp()
 
-function comp_padrao()
+  enemy_ref = state.enemy.list[state.enemy.index_comp]
 
-  enemy_ref = enen_list[state.enemy.index_comp]
-
-  -- ativar animacao/avanco da contagem
+  -- activate animation / current counter / random behavior 
   if enemy_ref.comp ~= "dead" then 
     state.enemy.counter[state.enemy.index_comp].anim_numb = 2 -- mudando animcao do item da contagem
+    -- random behavior
+    if state.enemy.index_comp <= #state.enemy.list and enemy_ref.change_comp == "on" then
+      enemy_ref.comp = state.enemy.comp_random[love.math.random(1, 3)] -- chosse bahavior
+      enemy_ref.change_comp = "off" -- stop behavior change
+    end
   end 
 
-  -- random behavior
-  if state.enemy.index_comp <= #enen_list and enemy_ref.change_comp == "on" then
-    enemy_ref.comp = state.enemy.comp_random[love.math.random(1, 3)] -- escolha de comportamento
-    enemy_ref.change_comp = "off" -- interrompendo mudança de comportamento
+  -- comp activation functions
+  if enemy_ref.comp == "stop" then
+    enemy_ref.main_anim = 1
+    comp_stop()
   end
-
-  -- lista de comportamentos
-  if state.turn ~= "off" then
-    
-    if enemy_ref.comp == "stop" then
-      enemy_ref.main_anim = 1
-      comp_stop()
-    end
-    if enemy_ref.comp == "path_diago" then
-      enemy_ref.main_anim = 3
-      comp_path_diago()
-    end
-    if enemy_ref.comp == "sleep" then
-      enemy_ref.main_anim = 4
-      comp_sleep()
-    end
-    if enemy_ref.comp == "alert_desconf" then
-      enemy_ref.main_anim = 3
-      alert_descon()
-    end
-    if enemy_ref.comp == "alert_player" or enemy_ref.comp == "alert_body" or enemy_ref.comp == "alert_item" then
-      enemy_ref.main_anim = 3
-      alert_player()
-      state.enemy.alert_d_time = 0 -- empty in case alert player is set
-    end
-    if enemy_ref.comp == "dead" then
-      enemy_ref.main_anim = 4
-      enemy_ref.alert_anim = 1 
-      comp_dead()
-    end
-    if enemy_ref.comp == "confuse" then 
-      comp_stop()
-      enemy_ref.alert_anim = 6
-    end
-
+  if enemy_ref.comp == "path_diago" then
+    enemy_ref.main_anim = 3
+    comp_path_diago()
+  end
+  if enemy_ref.comp == "sleep" then
+    enemy_ref.main_anim = 4
+    comp_sleep()
+  end
+  if enemy_ref.comp == "alert_desconf" then
+    enemy_ref.main_anim = 3
+    enemy_ref.change_comp = "off"
+    alert_descon()
+  end
+  if enemy_ref.comp == "alert_player" or enemy_ref.comp == "alert_body" or enemy_ref.comp == "alert_item" then
+    enemy_ref.main_anim = 3
+    enemy_ref.change_comp = "off"
+    alert_player()
+  end
+  if enemy_ref.comp == "dead" then
+    enemy_ref.main_anim = 4
+    enemy_ref.alert_anim = 1 
+    comp_dead()
+  end
+  if enemy_ref.comp == "confuse" then 
+    comp_stop()
+    enemy_ref.alert_anim = 6
+  end
+  
+  -- combat trigger
+  if ma_he(state.player,enemy_ref) == 45 and enemy_ref.comp == "alert_player" then 
+    state.combat.enemy_active = "on"
+    combate_update()
   end
 
 end
